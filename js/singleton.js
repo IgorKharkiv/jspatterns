63a5e5a187a0f404c6ad2aa5ee8792bd031c4432
igor.syrov <syrov.igor1988@gmail.com>
2017-04-16 21:17:44 +0300

    first commit

---
+++ js/singleton.js
@@ -0,0 +1,21 @@
+
+function Config(){
+
+  // check does Config has instance property
+  if( typeof Config.instance === 'object' ){
+    return Config.instance; // return if !!instance
+  }
+
+  this.someOption = 'Some option';
+
+  // save instance for repeat use
+  Config.instance = this;
+}
+
+
+// check our pattern
+
+var conf1 = new Config();
+var conf2 = new Config();
+
+console.log( conf1 === conf2); // true
